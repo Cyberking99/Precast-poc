3b5e6dd0cc66f5bb00557e301451f56ae5e63268
Kefas Kingsley <kefasiceking@gmail.com>
2026-01-05 13:21:05 +0100

    feat: add required state variables

--- src/LMSRPredictionMarket.sol
+++ src/LMSRPredictionMarket.sol
@@ -1,4 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract LMSRPredictionMarket {}
+contract LMSRPredictionMarket {
+    uint256 public b;
+    uint256 public qYES;
+    uint256 public qNO;
+
+    mapping(address => uint256) public yesShares;
+    mapping(address => uint256) public noShares;
+
+    bool public resolved;
+    bool public yesWon;
+}
